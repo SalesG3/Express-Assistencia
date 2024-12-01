09086ac98f87b261c785487ec66701bc24f3e86e
SalesG3 <contabilgs3@gmail.com>
2024-12-01 12:56:58 -0300

    Componente: Andamentos; Dados para Grid. OK!

--- routes/andamento.js
+++ routes/andamento.js
@@ -1,23 +1,37 @@
 const { app, conn } = require('../server.js');
 
-app.get('/codigo/andamento', async(req, res) => {
-    
-    let [query] = await conn.promise().execute(``)
-});
+// Dados para GRID: Andamentos
+app.post('/grid/andamento', async(req, res) => {
+    let { pesquisa } = req.body;
 
+    pesquisa = pesquisa ? String(pesquisa).replaceAll(' ','|') : '|';
+
+    let [query] = await conn.promise().execute('CALL grid_andamento ( ? )',
+        [pesquisa]
+    );
+
+    res.send(query[0])
+})
+
+// Novo Registro: Andamentos
 app.post('/novo/andamento', async(req, res) => {
     let { abertura, data, tecnico, registros } = req.body;
 
     let [query] = await conn.promise().query(`CALL novo_andamento ( ?, ? , ?)`,
         [abertura, data, tecnico]
     );
-    id = query[0][0].ID
 
+    if(query[0][0].duplicado){
+        res.send({ duplicado : query[0] });
+        return
+    };
+
+    id_andamento = query[0][0].id_andamento;
     for(let i = 0; i < registros.length; i++){
         [query] = await conn.promise().query(`CALL novo_executado ( ?, ?, ?, ?, ?, ?)`,
-            [id, registros[i].tipo, registros[i].executado , registros[i].quantidade, registros[i].desconto, registros[i].liquido]
+            [id_andamento, registros[i].tipo, registros[i].executado , registros[i].quantidade, registros[i].desconto, registros[i].liquido]
         );
     };
 
-    res.send(query);
+    res.send({ sucesso : query });
 })
